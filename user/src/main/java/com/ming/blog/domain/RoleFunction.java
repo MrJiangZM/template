f6d58f3d84bf52c5cf455d89774adc2b1675beb3
0002693 <jiangzaiming@mininglamp.com>
2020-03-30 14:09:48 +0800

    修改报错

--- user/src/main/java/com/ming/blog/domain/RoleFunction.java
+++ user/src/main/java/com/ming/blog/domain/RoleFunction.java
@@ -5,8 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.io.Serializable;
 
 /**
@@ -21,6 +20,8 @@ import java.io.Serializable;
 @Table(name = "role_function")
 public class RoleFunction implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private Integer rid;
     private Integer fid;
